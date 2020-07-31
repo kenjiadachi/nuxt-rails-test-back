b5bf615742437cde21d54a719ef0fd4e57c0205c
安達 健二 <kenji.183927@gmail.com>
2020-07-31 17:37:06 +0900

    修正

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,16 +1,16 @@
 class UsersController < ApplicationController
   before_action :set_user, only: [:show, :update, :destroy]
 
-  # GET /users
-  def index
-    @users = User.all
-    render json: @users
-  end
+  # # GET /users
+  # def index
+  #   @users = User.all
+  #   render json: @users
+  # end
 
-  # GET /users/1
-  def show
-    render json: @user
-  end
+  # # GET /users/1
+  # def show
+  #   render json: @user
+  # end
 
   # POST /users
   def create
@@ -22,19 +22,19 @@ class UsersController < ApplicationController
     end
   end
 
-  # PATCH/PUT /users/1
-  def update
-    if @user.update(user_params)
-      render json: @user
-    else
-      render json: @user.errors, status: :unprocessable_entity
-    end
-  end
+  # # PATCH/PUT /users/1
+  # def update
+  #   if @user.update(user_params)
+  #     render json: @user
+  #   else
+  #     render json: @user.errors, status: :unprocessable_entity
+  #   end
+  # end
 
-  # DELETE /users/1
-  def destroy
-    @user.destroy
-  end
+  # # DELETE /users/1
+  # def destroy
+  #   @user.destroy
+  # end
 
   private
 
